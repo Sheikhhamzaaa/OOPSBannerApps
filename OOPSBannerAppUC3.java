1bd5b1b230e8a827b6b1dbf9c499fbeb9e2c6a80
sheikhhamzaaa <thehsheikhhamza@gmail.com>
2026-03-10 13:59:32 +0530

    Added OOPS Banner App

---
+++ OOPSBannerAppUC3.java
@@ -0,0 +1,22 @@
+public class OOPSBannerAppUC3 {
+    public static void main(String[] args) {
+        System.out.println(" ***** ");
+        System.out.println(" ***** ");
+        System.out.println(" ******");
+        System.out.println(" ***** ");
+
+        System.out.println();
+
+        System.out.println("*     *");
+        System.out.println("*     *");
+        System.out.println("*     *");
+        System.out.println("*     *");
+
+        System.out.println();
+
+        System.out.println("*     *");
+        System.out.println("*     *");
+        System.out.println("*  *  *");
+        System.out.println("*     *");
+    }
+}
